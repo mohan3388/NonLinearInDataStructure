bdc23556f3a5d8a784587c9dbe3d98ec8aa6c95f
mohan338 <sahumohan338@gmail.com>
2022-05-05 18:50:28 +0530

    [mohan]add:calculate frequncy

---
+++ HashDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using HashDemo;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+        MyMapNode<string, int> hash = new MyMapNode<string, int>(5);
+        string words = "to be or not to be";
+        string[] arr = words.Split(' ');
+        LinkedList<string> checkForDuplication = new LinkedList<string>();
+        foreach (string element in arr) // to -> be
+        {
+            int count = 0;
+            foreach (string match in arr) //to->be->or->not->to-> be-> 
+            {
+                if (element == match)
+                {
+                    count++;//1->2
+                    if (checkForDuplication.Contains(element))
+                    {
+                        break;
+                    }
+                }
+
+            }
+
+            if (checkForDuplication.Contains(element))
+            {
+                continue;
+            }
+            checkForDuplication.AddLast(element);
+            hash.Add(element, count);//(to,2)
+        }
+       
+        hash.Display();
+    }
+}
